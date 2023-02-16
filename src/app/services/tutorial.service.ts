3b5bf2efd5cc0938e1dc76e90b58c852dde638f4
Mohammed Aamir <m.aamir@domain.rage-india.com>
2023-02-16 10:41:45 +0530

    Initial commit

---
+++ src/app/services/tutorial.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tutorial } from '../models/tutorial.model';
+
+const baseUrl = 'http://localhost:8081/api/tutorials';
+
+const headers= new HttpHeaders()
+  .set('content-type', 'application/json')
+  .set('Access-Control-Allow-Origin', '*');
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TutorialService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(baseUrl, { 'headers': headers });
+  }
+
+  get(id: any): Observable<Tutorial> {
+    return this.http.get<Tutorial>(`${baseUrl}/${id}`, { 'headers': headers });
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data, { 'headers': headers });
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data, { 'headers': headers });
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`, { 'headers': headers });
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl, { 'headers': headers });
+  }
+
+  findByTitle(title: any): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`, { 'headers': headers });
+  }
+}
